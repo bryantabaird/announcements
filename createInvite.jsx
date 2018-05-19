240a5b957738c8e9e33d9afb17dff711913530a5
bryantabaird <bryantabaird@gmail.com>
2018-05-19 09:59:16 -0600

    Consolidated file

--- createInvite.jsx
+++ createInvite.jsx
@@ -1,7 +1,6 @@
 ﻿#target indesign
 
 #include "json2.js"
-#include "functions.jsx"
 
 var dir = "/Users/baird/Library/Preferences/Adobe InDesign/Version 13.0/en_US/Scripts/Scripts Panel/";
 
@@ -18,8 +17,8 @@ myDocument.viewPreferences.verticalMeasurementUnits = MeasurementUnits.INCHES;
 // Set document preferences
 var myDocumentPreferences = myDocument.documentPreferences;
 myDocumentPreferences.facingPages = false;
-myDocumentPreferences.pageWidth = content.pageWidth;
-myDocumentPreferences.pageHeight = content.pageHeight;
+myDocumentPreferences.pageWidth = content["pageWidth"];
+myDocumentPreferences.pageHeight = content.["pageHeight"];
 
 // Sets bleed on all edges (others follow top when documentBleedUniformSize is true)
 myDocumentPreferences.documentBleedTopOffset = content.bleedSize;
@@ -40,14 +39,86 @@ for (var i = 0; i < pages.length; i++) {
     }
 
     // Set margin preferences
-    if (content.marginPreferences) {
+    if (content["marginPreferences"]) {
         // Loop through top, left, right, and bottom margin lengths
         for (var key in content.marginPreferences) {
-            myPage.marginPreferences[key] = content.marginPreferences[key];
+            myPage.marginPreferences[key] = content["marginPreferences"][key];
         }
     }
 
     if (page) {
         addPageItems(myPage, page);
     }    
+}
+
+// Help functions
+
+function addPageItems(page, content) {
+    // Loop through all pageItems
+    for (var contentKey in content) {
+        var pageItems = content[contentKey];
+
+        // Loop through each pageItem
+        for (var i = 0; i < pageItems.length; i++) {
+            var pageItem = pageItems[i];
+
+            // Optionally add pageItem properties to frame
+            if (pageItem["properties"])
+                var frame = page[contentKey].add(pageItem["properties"]);
+
+            // Optionally run specified methods
+            if (pageItem["methods"]) {
+                var methods = pageItem["methods"];
+                for (var j = 0; j < methods.length; j++) {
+                    var method = pageItem["methods"][j];
+
+                    // Handle special case of type conversion for image file
+                    // Ideal to modify JSON inputs if there are many function calls
+                    if (method["name"] === "place")
+                        method["params"][0] = new File(method["params"][0])
+
+                    // For now, only 1 parameter supported
+                    frame[method["name"]](method["params"][0]);
+                }
+            }
+        }
+    }    
+}
+
+function evaluateJSONObject(obj) {
+    for (var key in obj) {
+
+        // Ignore object prototype properties
+        if (!obj.hasOwnProperty(key))
+            continue;
+
+        // Recurse if object value is another object
+        if (obj[key] !== null && typeof obj[key] == "object") {
+            evaluateJSONObject(obj[key]);
+        }
+
+        // Evaluate the json property value, ignoring the "None" literal
+        else {
+            try {
+                obj[key] = eval(obj[key])
+            } catch (e) {
+                // If the string can't be evaluated, do nothing
+            }
+        }
+    }
+
+    return obj;
+}
+
+function getJSONFromFile(fileLocation) {
+    var fileContent = readFileContent(fileLocation)
+    return evaluateJSONObject(JSON.parse(fileContent));
+}
+
+function readFileContent(fileLocation) {
+    var scriptFile = File(fileLocation);
+    scriptFile.open('r');  
+    var content = scriptFile.read();  
+    scriptFile.close();
+    return content;
 }
